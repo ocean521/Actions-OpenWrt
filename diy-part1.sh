6c5ad80703472ae0b8279248f822c8ab6d4dd646
ocean521 <61226271@qq.com>
2020-06-21 07:52:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,6 +11,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
